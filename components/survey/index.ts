3cad40dbca1e89c9a7ebea1bcaa3b81a97ffb788
Ti-V4-Saman <felipesamanbh@gmail.com>
2026-07-05 01:15:28 -0300

    feat: add QuestionCard main composite and barrel exports

---
+++ components/survey/index.ts
@@ -0,0 +1,20 @@
+export { StepIndicator } from "./step-indicator"
+export { QuestionHeader } from "./question-header"
+export { ChoiceCard } from "./choice-card"
+export { OptionList } from "./option-list"
+export { QuestionTextInput } from "./question-text-input"
+export { QuestionDropdown } from "./question-dropdown"
+export { QuestionFooter } from "./question-footer"
+export { QuestionCard } from "./question-card"
+export type {
+  QuestionType,
+  SurveyOption,
+  QuestionCardProps,
+  ChoiceCardProps,
+  OptionListProps,
+  QuestionTextInputProps,
+  QuestionDropdownProps,
+  StepIndicatorProps,
+  QuestionHeaderProps,
+  QuestionFooterProps,
+} from "./types"
